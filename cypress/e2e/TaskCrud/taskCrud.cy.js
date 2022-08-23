25b625e1e66e2c155c78997a891b922fcb5842f9
alihaider121 <ali.haider6713@gmail.com>
2022-08-23 21:40:26 +0500

    new file

--- cypress/e2e/TaskCrud/taskCrud.cy.js
+++ cypress/e2e/TaskCrud/taskCrud.cy.js
@@ -1,6 +1,6 @@
 describe("Task complete test cases", () => {
     it("task is added marekd completed ", () => {
      cy.visit("/");
-     cy.url().should('not.contain', "google");
+     cy.url().should('contain', "google");
     });
 });
